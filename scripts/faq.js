89145c821ab01018a6ce3ffdddfe95e8811707d9
John <barbalata.ionut@gmail.com>
2018-03-26 21:03:02 +0300

    Add files via upload

---
+++ scripts/faq.js
@@ -0,0 +1,13 @@
+
+$(function() {
+  $(".expand").on( "click", function() {
+    $(this).next().slideToggle(200);
+    $expand = $(this).find(">:first-child");
+
+    if($expand.text() == "+") {
+      $expand.text("-");
+    } else {
+      $expand.text("+");
+    }
+  });
+});
